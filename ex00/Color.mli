bf3f48ec437ffa236b1d1662c7a6cfff450ce571
ykusano <110902237+deerman31@users.noreply.github.com>
2025-08-31 12:57:49 +0900

    ColorとValueモジュールのインターフェースファイルを追加し、Makefileを更新してインターフェースファイルのコンパイルを追加

---
+++ ex00/Color.mli
@@ -0,0 +1,5 @@
+type t = Spade | Heart | Diamond | Club
+
+val all : t list
+val toString : t -> string
+val toStringVerbose : t -> string
--- ex01/Value.ml
+++ ex01/Value.ml
@@ -1,98 +1,98 @@
-module Value = struct
-  type t =
-    | T2
-    | T3
-    | T4
-    | T5
-    | T6
-    | T7
-    | T8
-    | T9
-    | T10
-    | Jack
-    | Queen
-    | King
-    | As
+(* module Value = struct *)
+type t =
+  | T2
+  | T3
+  | T4
+  | T5
+  | T6
+  | T7
+  | T8
+  | T9
+  | T10
+  | Jack
+  | Queen
+  | King
+  | As
 
-  let all = [ T2; T3; T4; T5; T6; T7; T8; T9; T10; Jack; Queen; King; As ]
+let all = [ T2; T3; T4; T5; T6; T7; T8; T9; T10; Jack; Queen; King; As ]
 
-  let toInt t =
-    match t with
-    | T2 -> 2
-    | T3 -> 3
-    | T4 -> 4
-    | T5 -> 5
-    | T6 -> 6
-    | T7 -> 7
-    | T8 -> 8
-    | T9 -> 9
-    | T10 -> 10
-    | Jack -> 11
-    | Queen -> 12
-    | King -> 13
-    | As -> 1
+let toInt t =
+  match t with
+  | T2 -> 2
+  | T3 -> 3
+  | T4 -> 4
+  | T5 -> 5
+  | T6 -> 6
+  | T7 -> 7
+  | T8 -> 8
+  | T9 -> 9
+  | T10 -> 10
+  | Jack -> 11
+  | Queen -> 12
+  | King -> 13
+  | As -> 1
 
-  let toString t =
-    match t with
-    | T2 -> "2"
-    | T3 -> "3"
-    | T4 -> "4"
-    | T5 -> "5"
-    | T6 -> "6"
-    | T7 -> "7"
-    | T8 -> "8"
-    | T9 -> "9"
-    | T10 -> "10"
-    | Jack -> "J"
-    | Queen -> "Q"
-    | King -> "K"
-    | As -> "A"
+let toString t =
+  match t with
+  | T2 -> "2"
+  | T3 -> "3"
+  | T4 -> "4"
+  | T5 -> "5"
+  | T6 -> "6"
+  | T7 -> "7"
+  | T8 -> "8"
+  | T9 -> "9"
+  | T10 -> "10"
+  | Jack -> "J"
+  | Queen -> "Q"
+  | King -> "K"
+  | As -> "A"
 
-  let toStringVerbose t =
-    match t with
-    | T2 -> "2"
-    | T3 -> "3"
-    | T4 -> "4"
-    | T5 -> "5"
-    | T6 -> "6"
-    | T7 -> "7"
-    | T8 -> "8"
-    | T9 -> "9"
-    | T10 -> "10"
-    | Jack -> "Jack"
-    | Queen -> "Queen"
-    | King -> "King"
-    | As -> "As"
+let toStringVerbose t =
+  match t with
+  | T2 -> "2"
+  | T3 -> "3"
+  | T4 -> "4"
+  | T5 -> "5"
+  | T6 -> "6"
+  | T7 -> "7"
+  | T8 -> "8"
+  | T9 -> "9"
+  | T10 -> "10"
+  | Jack -> "Jack"
+  | Queen -> "Queen"
+  | King -> "King"
+  | As -> "As"
 
-  let next t =
-    match t with
-    | T2 -> T3
-    | T3 -> T4
-    | T4 -> T5
-    | T5 -> T6
-    | T6 -> T7
-    | T7 -> T8
-    | T8 -> T9
-    | T9 -> T10
-    | T10 -> Jack
-    | Jack -> Queen
-    | Queen -> King
-    | King -> As
-    | As -> invalid_arg "The next value of AS does not exist"
+let next t =
+  match t with
+  | T2 -> T3
+  | T3 -> T4
+  | T4 -> T5
+  | T5 -> T6
+  | T6 -> T7
+  | T7 -> T8
+  | T8 -> T9
+  | T9 -> T10
+  | T10 -> Jack
+  | Jack -> Queen
+  | Queen -> King
+  | King -> As
+  | As -> invalid_arg "The next value of AS does not exist"
 
-  let previous t =
-    match t with
-    | T2 -> invalid_arg "The previous value of T2 does not exist"
-    | T3 -> T2
-    | T4 -> T3
-    | T5 -> T4
-    | T6 -> T5
-    | T7 -> T6
-    | T8 -> T7
-    | T9 -> T8
-    | T10 -> T9
-    | Jack -> T10
-    | Queen -> Jack
-    | King -> Queen
-    | As -> King
-end
+let previous t =
+  match t with
+  | T2 -> invalid_arg "The previous value of T2 does not exist"
+  | T3 -> T2
+  | T4 -> T3
+  | T5 -> T4
+  | T6 -> T5
+  | T7 -> T6
+  | T8 -> T7
+  | T9 -> T8
+  | T10 -> T9
+  | Jack -> T10
+  | Queen -> Jack
+  | King -> Queen
+  | As -> King
+(* end *)
